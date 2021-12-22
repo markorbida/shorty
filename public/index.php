0914de2a916339b27c626d80e1e45f00598f9b46
Mark Orbida <mark@orbida.com>
2021-12-22 22:02:33 +0000

    Moved index to public folder

--- public/index.php
+++ public/index.php
@@ -1,12 +1,28 @@
 <?php
-require './shorty.php';
-require './config.php';
 
-$shorty = new Shorty($hostname, $connection);
+require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/../shorty.php';
 
-$shorty->set_chars($chars);
-$shorty->set_salt($salt);
-$shorty->set_padding($padding);
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+
+try {
+    $connection = new PDO("mysql:dbname={$_ENV['DB_NAME']};host={$_ENV['DB_HOST']}", $_ENV['DB_USER'], $_ENV['DB_PASS']);
+} catch (PDOException $ex) {
+    die(json_encode(array(
+        'error' => true,
+        'message' => 'Unable to connect to db.'
+    )));
+}
+
+$shorty = new Shorty($_ENV['BASE_URL'], $connection);
+
+if(isset($_ENV['AUTH_CREDS'])){
+    $shorty->set_creds(json_decode($_ENV['AUTH_CREDS'], true));
+}
+
+$shorty->set_chars($_ENV['CHARS']);
+$shorty->set_salt($_ENV['SALT']);
+$shorty->set_padding($_ENV['PADDING']);
 
 $shorty->run();
-?>
