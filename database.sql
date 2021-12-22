0b680f0ca9d3a0a0c6224cc553193a1b3993bb57
Mark Orbida <mark@orbida.com>
2021-12-22 22:01:43 +0000

    Added new columns

--- database.sql
+++ database.sql
@@ -1,12 +1,18 @@
 #
 # Table schema for MySQL
 #
-CREATE TABLE urls (
-    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    url VARCHAR(1000) NOT NULL,
-    created DATETIME NOT NULL,
-    accessed DATETIME,
-    hits INT UNSIGNED NOT NULL DEFAULT 0,
-    PRIMARY KEY (id),
-    UNIQUE (url)
-);
+CREATE TABLE `urls` (
+	`id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+	`params` JSON NULL DEFAULT NULL,
+	`url` VARCHAR(1000) NOT NULL COLLATE 'latin1_swedish_ci',
+	`user` VARCHAR(320) AS (json_unquote(json_extract(`params`,'$.user'))) virtual,
+	`hits` INT(10) UNSIGNED NULL DEFAULT '0',
+	`created` DATETIME NULL DEFAULT CURRENT_TIMESTAMP,
+	`accessed` DATETIME NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+	PRIMARY KEY (`id`) USING BTREE,
+	UNIQUE INDEX `url_user` (`url`, `user`) USING BTREE
+)
+COLLATE='latin1_swedish_ci'
+ENGINE=InnoDB
+AUTO_INCREMENT=16
+;
